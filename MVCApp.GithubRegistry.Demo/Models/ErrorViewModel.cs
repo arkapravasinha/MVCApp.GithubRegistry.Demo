9f4ae69a6501cb03e18776ac4d4315fb41ce439c
Arka <arkaprava.123040@gmail.com>
2021-05-06 15:10:10 +0530

    Add project files.

---
+++ MVCApp.GithubRegistry.Demo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCApp.GithubRegistry.Demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
